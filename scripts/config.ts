971f0a3ada6d24dfaa19e0fc8287063b63742e12
rendyelang <lsmnrndy123@gmail.com>
2025-11-13 17:52:13 +0700

    change fetch config.json url

--- scripts/config.ts
+++ scripts/config.ts
@@ -6,12 +6,14 @@ export async function getApiBaseUrl(): Promise<string> {
   if (API_BASE_URL) return API_BASE_URL; // cache biar gak fetch berulang
 
   try {
-    const res = await fetch("https://github.com/rendyelang/restau_api_url_ngrok/blob/main/config.json");
+    const res = await fetch("https://raw.githubusercontent.com/rendyelang/restau_api_url_ngrok/main/config.json");
     const data = await res.json();
     API_BASE_URL = data.API_URL;
+    console.log("berhasil", API_BASE_URL);
     return API_BASE_URL;
   } catch (error) {
     console.warn("⚠️ Gagal fetch config.json, pakai fallback .env");
+    console.error(error);
     return process.env.EXPO_PUBLIC_API_URL || "";
   }
 }
